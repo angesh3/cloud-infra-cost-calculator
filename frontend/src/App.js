72259e8681840cd638bfa96dae402463bf1abcbb
angesh3 <angesh@gmail.com>
2025-04-14 01:08:22 -0700

    Remove Pricing Configuration tab and unused component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Box, Container, Tab, Tabs } from '@mui/material';
-import PricingConfig from './components/PricingConfig';
+import { Box, Container } from '@mui/material';
 import CostCalculator from './components/CostCalculator';
 
 function App() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   return (
     <Container maxWidth="lg">
       <Box sx={{ width: '100%', mt: 3 }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange}>
-            <Tab label="Cost Calculator" />
-            <Tab label="Pricing Configuration" />
-          </Tabs>
-        </Box>
         <Box sx={{ mt: 3 }}>
-          {value === 0 && <CostCalculator />}
-          {value === 1 && <PricingConfig />}
+          <CostCalculator />
         </Box>
       </Box>
     </Container>
